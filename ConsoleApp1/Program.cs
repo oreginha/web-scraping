d8dc4c343ee4064a953eb3e6d2faca9d27c8c6ab
Facundo Alvarez <falvarezgarriga@gmail.com>
2025-01-09 13:13:49 -0300

    scraping con errores

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,8 @@ using System.Diagnostics;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System.Linq;
+using System.Text;
+using System.Collections.Generic;
 
 namespace EventScraperBackend
 {
@@ -13,11 +15,10 @@ namespace EventScraperBackend
         static void Main(string[] args)
         {
             // Cerrar consolas existentes
-         
+
             IWebDriver driver = null;
             try
             {
-
                 // Configuracion
                 ChromeOptions options = new ChromeOptions();
                 options.AddArgument("--headless");
@@ -26,51 +27,181 @@ namespace EventScraperBackend
                 options.AddArgument("--disable-dev-shm-usage");
                 options.AddArgument("--remote-debugging-port=9222");
 
-
                 // Inicialización
                 driver = new ChromeDriver("C:\\SeleniumDrivers\\chromedriver");
-
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
                 driver.Navigate().GoToUrl("https://www.ticketek.com.ar/musica");
 
                 var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
                 // Buscar un elemento
-                var contenedores = driver.FindElements(By.CssSelector("div.tkt-artist-list-image-item.relative.col-xs-10.col-sm-33.col-md-10"));
+                IReadOnlyCollection<IWebElement> contenedoresIniciales = wait.Until(ExpectedConditions
+                    .PresenceOfAllElementsLocatedBy(By.CssSelector("div.tkt-artist-list-image-item.relative.col-xs-10")));
 
-                Console.WriteLine(contenedores.Count);
+                Console.WriteLine($"Total de contenedores encontrados: {contenedoresIniciales.Count}");
 
-                foreach (var container in contenedores)
+                foreach (var contenedorInicial in contenedoresIniciales)
                 {
+                    IWebElement container = contenedorInicial;
+                    string imageUrl = null;
+                    string name = null;
+                    string url = null;
+                    string imageUrlDetail = null;
+                    string description = null;
+                    string date = null;
+                    string place = null;
+                    string finallocationDiv = null;
+                    string buyUrl = null;
                     try
                     {
-                        // Imprimir el HTML del contenedor para inspección
-                        //var containerHTML = container.GetAttribute("innerHTML");
-                        //Console.WriteLine("HTML contenedor: " + containerHTML);
+                        IWebElement imageElement = null;
+                        try
+                        {
+                            imageElement = container.FindElement(By.CssSelector("img.img-responsive.tkt-img-info.col-xs-10"));
+
+                            // Extraer la URL de la imagen y el nombre
+                            imageUrl = imageElement.GetAttribute("src");
+                            name = imageElement.GetAttribute("alt");
+
+                        }
+                        catch (NoSuchElementException ex)
+                        {
+                            Console.WriteLine($"Error al obtener la imagen y el nombre: {ex.Message}");
+                        }
 
 
-                        // Extraer la URL de la imagen y el nombre
-                        var imageElement = container.FindElement(By.CssSelector("img.img-responsive.tkt-img-info.col-xs-10.no-padding"));
-                        var imageUrl = imageElement.GetAttribute("src");
-                        var name = imageElement.GetAttribute("alt");
-                        Console.WriteLine(imageUrl);
-                        Console.WriteLine(name);
+                        IWebElement boton = null;
+                        try
+                        {
+                            boton = container.FindElement(By.CssSelector("a"));
+                            url = boton.GetAttribute("href");
+                        }
+                        catch (NoSuchElementException ex)
+                        {
+                            Console.WriteLine($"Error al obtener la URL: {ex.Message}");
+                        }
 
-                    }
-                    catch (NoSuchElementException e)
-                    {
-                        Console.WriteLine($"No se pudo encontrar el elemento: {e.Message}");
+                        // Abre una nueva pestaña
+                        string originalWindow = driver.CurrentWindowHandle;
+                        driver.SwitchTo().NewWindow(WindowType.Tab);
+
+                        // Navega a la página de detalles en la nueva pestaña
+                        if (url != null)
+                        {
+                            driver.Navigate().GoToUrl(url);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se puede navegar a la URL de la pagina de detalle porque no se pudo obtener");
+                            driver.Close();
+                            driver.SwitchTo().Window(originalWindow);
+                            continue;
+                        }
+                        // Esperar a que cargue la página
+                        try
+                        {
+                            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("body.os-windows.browser-chrome.device-unknown")));
+                        }
+                        catch (WebDriverTimeoutException ex)
+                        {
+                            Console.WriteLine($"Error al esperar la pagina de detalle: {ex.Message}");
+                            driver.Close();
+                            driver.SwitchTo().Window(originalWindow);
+                            continue;
+
+                        }
+                        IWebElement imageElementDetailObject = null;
+                        try
+                        {
+                            // Extraer la URL de la imagen grande
+                            imageElementDetailObject = driver.FindElement(By.CssSelector("img.img-responsive"));
+                            imageUrlDetail = imageElementDetailObject.GetAttribute("src");
+                        }
+                        catch (NoSuchElementException ex)
+                        {
+                            Console.WriteLine($"Error al obtener la imagen de detalle: {ex.Message}");
+                        }
+
+                        IWebElement descriptionElementObject = null;
+                        try
+                        {
+                            // Extraer la descripción
+                            descriptionElementObject = driver.FindElement(By.CssSelector("p"));
+                            description = descriptionElementObject.Text;
+
+                        }
+                        catch (NoSuchElementException ex)
+                        {
+                            Console.WriteLine($"Error al obtener la descripcion: {ex.Message}");
+                        }
+                        IWebElement dateElementObject = null;
+                        try
+                        {
+                            // Extraer la fecha
+                            dateElementObject = driver.FindElement(By.CssSelector("strong"));
+                            date = dateElementObject.Text;
+                        }
+                        catch (NoSuchElementException ex)
+                        {
+                            Console.WriteLine($"Error al obtener la fecha: {ex.Message}");
+                        }
+
+                        IWebElement placeElementObject = null;
+                        try
+                        {
+                            // Extraer el lugar
+                            placeElementObject = driver.FindElement(By.CssSelector("h4.text-uppercase"));
+                            place = placeElementObject.Text;
+                        }
+                        catch (NoSuchElementException ex)
+                        {
+                            Console.WriteLine($"Error al obtener el lugar: {ex.Message}");
+                        }
+                        IReadOnlyCollection<IWebElement> locationDivs = null;
+                        try
+                        {
+                            //Extraer direccion
+                            locationDivs = wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("div.text-black")));
+                            StringBuilder concatenatedText = new StringBuilder();
+                            foreach (IWebElement locationDiv in locationDivs)
+                            {
+                                concatenatedText.Append(locationDiv.Text);
+                                concatenatedText.Append(" "); // Add a space between each div for readability
+                            }
+                            finallocationDiv = concatenatedText.ToString().Trim();
+                        }
+                        catch (NoSuchElementException ex)
+                        {
+                            Console.WriteLine($"Error al obtener la direccion: {ex.Message}");
+                        }
+
+                        IWebElement buyElementObject = null;
+                        try
+                        {
+                            // Extraer el link de compra
+                            buyElementObject = driver.FindElement(By.CssSelector("a.btn.btn-default.show-btn.show-buy.animate"));
+                            buyUrl = buyElementObject.GetAttribute("href");
+
+                        }
+                        catch (NoSuchElementException ex)
+                        {
+                            Console.WriteLine($"Error al obtener la url de compra: {ex.Message}");
+                        }
+                        //Console.WriteLine($"Elementos extraídos: Nombre: '{name}', Imagen: '{imageUrl}', URL: '{url}', Imagen Detalle: '{imageUrlDetail}', Descripcion: '{description}', Fecha: '{date}', Lugar: '{place}', Direccion: '{finallocationDiv}', URL Compra: '{buyUrl}'");
+
+                        // Cerrar la pestaña actual
+                        driver.Close();
+                        driver.SwitchTo().Window(originalWindow);
+
                     }
                     catch (Exception ex)
                     {
-                        Console.WriteLine($"Ocurrio un error: {ex.Message}");
+                        Console.WriteLine($"Error en iteración (general): {ex.Message}");
                     }
                 }
-
-
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Ocurrio un error: {ex.Message}");
+                Console.WriteLine($"Ocurrio un error (general): {ex.Message}");
             }
             finally
             {
@@ -78,7 +209,6 @@ namespace EventScraperBackend
                 {
                     driver.Quit();
                 }
-
             }
         }
     }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a20073b88017abbc1ba1a08ee5b90377c40097")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e3e6fd65efd74c2f197443b5ea4afa84056802")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
