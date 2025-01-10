84c9b7968f65cb92aef215f9e6eb4882d10135aa
Facundo Alvarez <falvarezgarriga@gmail.com>
2025-01-10 09:22:28 -0300

    antes de romper todo 2

--- ConsoleApp1/CORE/EventDataExtractor.cs
+++ ConsoleApp1/CORE/EventDataExtractor.cs
@@ -18,164 +18,165 @@ namespace EventScraperBackend.Core
             try
             {
                 IWebElement imageElement = ElementExtractor.FindElement(container, By.CssSelector("img.img-responsive.tkt-img-info.col-xs-10"), wait);
-                eventData.ImageUrl = ElementExtractor.ExtractAttribute(imageElement, "src");
-                eventData.Name = ElementExtractor.ExtractAttribute(imageElement, "alt");
+                 eventData.ImageUrl = ElementExtractor.ExtractAttribute(imageElement, "src");
+                 eventData.Name = ElementExtractor.ExtractAttribute(imageElement, "alt");
 
 
-                IWebElement boton = ElementExtractor.FindElement(container, By.CssSelector("a"), wait);
+                 IWebElement boton = ElementExtractor.FindElement(container, By.CssSelector("a"), wait);
                 eventData.Url = ElementExtractor.ExtractAttribute(boton, "href");
                 // Abre una nueva pestaña
-                string originalWindow = driver.CurrentWindowHandle;
+                 string originalWindow = driver.CurrentWindowHandle;
                 NavigationHelper.OpenNewTabAndNavigate(driver, url);
                 bool footerExists = false;
-                try
-                {
+                  try
+                 {
                     var footer = driver.FindElement(By.CssSelector("tkt-image-show no-padding.text-uppercase.col-xs-10 bg-grey"));
-                }
-                catch (Exception ex)
-                {
+                  }
+                   catch (Exception ex)
+                  {
 
 
-                    footerExists = true;
+                        footerExists = true;
 
-                }
-
-
-                if (footerExists)
-                {
-                    eventData = ProcessEventWithHrefs(driver, wait, url, originalWindow);
-                }
-                else
-                {
-                    eventData = ProcessEventWithoutHrefs(driver, wait, url, originalWindow);
-                }
+                    }
+               if (footerExists)
+                    {
+                       eventData = ProcessEventWithHrefs(driver, wait, url, originalWindow);
+                     }
+                    else
+                    {
+                        eventData = ProcessEventWithoutHrefs(driver, wait, url, originalWindow);
+                   }
 
                 NavigationHelper.CloseCurrentTabAndSwitchBack(driver, originalWindow);
 
             }
             finally
             {
-            }
+             }
             return eventData;
-        }
+         }
         private static EventData ProcessEventWithHrefs(IWebDriver driver, WebDriverWait wait, string url, string originalWindow)
         {
             EventData eventData = new EventData();
 
-            IReadOnlyCollection<IWebElement> hrefs = ElementExtractor.FindElements(By.CssSelector("#footer a[href]"), wait);
-            if (hrefs == null)
+               IReadOnlyCollection<IWebElement> hrefs = ElementExtractor.FindElements(By.CssSelector("#footer a[href]"), wait);
+              if (hrefs == null)
             {
-                NavigationHelper.CloseCurrentTabAndSwitchBack(driver, originalWindow);
-                return null;
+                 NavigationHelper.CloseCurrentTabAndSwitchBack(driver, originalWindow);
+                  return null;
             }
-            else
+           else
             {
 
-                foreach (var hrefElement in hrefs)
-                {
-                    try
+                 foreach (var hrefElement in hrefs)
                     {
-                        string href = ElementExtractor.ExtractAttribute(hrefElement, "href");
-                        // Abre una nueva pestaña
-                        string innerTabOriginalWindow = driver.CurrentWindowHandle;
-                        NavigationHelper.OpenNewTabAndNavigate(driver, href);
                         try
-                        {
-                            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("body.os-windows.browser-chrome.device-unknown")));
-                            IWebElement imageElementDetailObject = ElementExtractor.FindElement(null, By.CssSelector("img.img-responsive.col-sm-10"), wait);
-                            eventData.ImageUrlDetail = ElementExtractor.ExtractAttribute(imageElementDetailObject, "src");
-                            IWebElement descriptionElementObject = ElementExtractor.FindElement(null, By.CssSelector("p"), wait);
-                            eventData.Description = ElementExtractor.ExtractText(descriptionElementObject);
-                            IWebElement dateElementObject = ElementExtractor.FindElement(null, By.CssSelector("strong"), wait);
-                            eventData.Date = ElementExtractor.ExtractText(dateElementObject);
-                            IWebElement placeElementObject = ElementExtractor.FindElement(null, By.CssSelector("h4.text-uppercase"), wait);
-                            eventData.Place = ElementExtractor.ExtractText(placeElementObject);
-                            IReadOnlyCollection<IWebElement> locationDivs = ElementExtractor.FindElements(By.CssSelector("div.text-black"), wait);
-                            StringBuilder concatenatedText = new StringBuilder();
-                            foreach (IWebElement locationDiv in locationDivs)
-                            {
-                                concatenatedText.Append(locationDiv.Text);
-                                concatenatedText.Append(" "); // Add a space between each div for readability
-                            }
-                            eventData.FinallocationDiv = concatenatedText.ToString().Trim();
-                            IWebElement buyElementObject = ElementExtractor.FindElement(null, By.CssSelector("a.btn.btn-default.show-btn.show-buy.animate"), wait);
-                            eventData.BuyUrl = ElementExtractor.ExtractAttribute(buyElementObject, "href");
+                       {
+                            string href = ElementExtractor.ExtractAttribute(hrefElement, "href");
+                          // Abre una nueva pestaña
+                             string innerTabOriginalWindow = driver.CurrentWindowHandle;
+                            NavigationHelper.OpenNewTabAndNavigate(driver, href);
+                              try
+                                {
+                                 // Esperar a que la pagina este cargada.
+                                 WebDriverWait localWait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+                                  localWait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
 
-
-                            NavigationHelper.CloseCurrentTabAndSwitchBack(driver, innerTabOriginalWindow);
-                            return eventData;
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Error en iteración (href): {ex.Message}");
-                            NavigationHelper.CloseCurrentTabAndSwitchBack(driver, innerTabOriginalWindow);
-                            return null;
-                        }
-                        finally
-                        {
-                            NavigationHelper.CloseCurrentTabAndSwitchBack(driver, innerTabOriginalWindow);
-                        }
-                    }
-                    catch (Exception ex)
+                                    IWebElement imageElementDetailObject = ElementExtractor.FindElementWithoutWait(null, By.CssSelector("img.img-responsive.col-sm-10"));
+                                   eventData.ImageUrlDetail = ElementExtractor.ExtractAttribute(imageElementDetailObject, "src");
+                                   IWebElement descriptionElementObject = ElementExtractor.FindElementWithoutWait(null, By.CssSelector("p"));
+                                    eventData.Description = ElementExtractor.ExtractText(descriptionElementObject);
+                                  IWebElement dateElementObject = ElementExtractor.FindElementWithoutWait(null, By.CssSelector("strong"));
+                                     eventData.Date = ElementExtractor.ExtractText(dateElementObject);
+                                    IWebElement placeElementObject = ElementExtractor.FindElementWithoutWait(null, By.CssSelector("h4.text-uppercase"));
+                                     eventData.Place = ElementExtractor.ExtractText(placeElementObject);
+                                    IReadOnlyCollection<IWebElement> locationDivs = ElementExtractor.FindElements(By.CssSelector("div.text-black"), wait);
+                                     StringBuilder concatenatedText = new StringBuilder();
+                                    foreach (IWebElement locationDiv in locationDivs)
+                                     {
+                                      concatenatedText.Append(locationDiv.Text);
+                                         concatenatedText.Append(" "); // Add a space between each div for readability
+                                    }
+                                   eventData.FinallocationDiv = concatenatedText.ToString().Trim();
+                                     IWebElement buyElementObject = ElementExtractor.FindElementWithoutWait(null, By.CssSelector("a.btn.btn-default.show-btn.show-buy.animate"));
+                                     eventData.BuyUrl = ElementExtractor.ExtractAttribute(buyElementObject, "href");
+                                  NavigationHelper.CloseCurrentTabAndSwitchBack(driver, innerTabOriginalWindow);
+                                    return eventData;
+                                }
+                                 catch (Exception ex)
+                                {
+                                  Console.WriteLine($"Error en iteración (href): {ex.Message}");
+                                   NavigationHelper.CloseCurrentTabAndSwitchBack(driver, innerTabOriginalWindow);
+                                    return null;
+                                 }
+                            finally
+                           {
+                                NavigationHelper.CloseCurrentTabAndSwitchBack(driver, innerTabOriginalWindow);
+                           }
+                       }
+                   catch (Exception ex)
                     {
-                        Console.WriteLine($"Error en iteración (href): {ex.Message}");
+                       Console.WriteLine($"Error en iteración (href): {ex.Message}");
                         NavigationHelper.CloseCurrentTabAndSwitchBack(driver, originalWindow);
                         return null;
-                    }
+                   }
 
                 }
-                return eventData;
+                   return eventData;
 
-            }
+           }
         }
-        private EventData ProcessEventWithoutHrefs(IWebDriver driver, WebDriverWait wait, string url, string originalWindow)
+         private EventData ProcessEventWithoutHrefs(IWebDriver driver, WebDriverWait wait, string url, string originalWindow)
         {
             EventData eventData = new EventData();
             try
             {
                 // Esperar a que cargue la página
-                wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("body.os-windows.browser-chrome.device-unknown")));
-                IWebElement imageElementDetailObject = ElementExtractor.FindElement(null, By.CssSelector("img.img-responsive.col-sm-10"), wait);
+                WebDriverWait localWait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+                localWait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+
+
+                IWebElement imageElementDetailObject = ElementExtractor.FindElementWithoutWait(null, By.CssSelector("img.img-responsive.col-sm-10"));
                 eventData.ImageUrlDetail = ElementExtractor.ExtractAttribute(imageElementDetailObject, "src");
-                IWebElement descriptionElementObject = ElementExtractor.FindElement(null, By.CssSelector("p"), wait);
+                IWebElement descriptionElementObject = ElementExtractor.FindElementWithoutWait(null, By.CssSelector("p"));
                 eventData.Description = ElementExtractor.ExtractText(descriptionElementObject);
-                IWebElement dateElementObject = ElementExtractor.FindElement(null, By.CssSelector("strong"), wait);
+                 IWebElement dateElementObject = ElementExtractor.FindElementWithoutWait(null, By.CssSelector("strong"));
                 eventData.Date = ElementExtractor.ExtractText(dateElementObject);
-                IWebElement placeElementObject = ElementExtractor.FindElement(null, By.CssSelector("h4.text-uppercase"), wait);
-                eventData.Place = ElementExtractor.ExtractText(placeElementObject);
+                IWebElement placeElementObject = ElementExtractor.FindElementWithoutWait(null, By.CssSelector("h4.text-uppercase"));
+                 eventData.Place = ElementExtractor.ExtractText(placeElementObject);
 
                 IReadOnlyCollection<IWebElement> locationDivs = ElementExtractor.FindElements(By.CssSelector("div.text-black"), wait);
                 StringBuilder concatenatedText = new StringBuilder();
-                foreach (IWebElement locationDiv in locationDivs)
+               foreach (IWebElement locationDiv in locationDivs)
                 {
-                    concatenatedText.Append(locationDiv.Text);
+                   concatenatedText.Append(locationDiv.Text);
                     concatenatedText.Append(" "); // Add a space between each div for readability
                 }
                 eventData.FinallocationDiv = concatenatedText.ToString().Trim();
 
                 IWebElement buyElementObject = ElementExtractor.FindElementWithoutWait(null, By.CssSelector("a.btn.btn-default.show-btn.show-buy.animate"));
-                eventData.BuyUrl = ElementExtractor.ExtractAttribute(buyElementObject, "href");
-            }
-            catch (WebDriverTimeoutException ex)
-            {
-                Console.WriteLine($"Error al esperar la pagina de detalle: {ex.Message}");
+              eventData.BuyUrl = ElementExtractor.ExtractAttribute(buyElementObject, "href");
+           }
+          catch (WebDriverTimeoutException ex)
+           {
+               Console.WriteLine($"Error al esperar la pagina de detalle: {ex.Message}");
                 NavigationHelper.CloseCurrentTabAndSwitchBack(driver, originalWindow);
-                return null;
-            }
+               return null;
+           }
             NavigationHelper.CloseCurrentTabAndSwitchBack(driver, originalWindow);
-            return eventData;
+           return eventData;
         }
-
-        private void OpenNewTabAndNavigate(IWebDriver driver, string url)
+         private void OpenNewTabAndNavigate(IWebDriver driver, string url)
         {
             string originalWindow = driver.CurrentWindowHandle;
-            driver.SwitchTo().NewWindow(WindowType.Tab);
-            driver.Navigate().GoToUrl(url);
-        }
+           driver.SwitchTo().NewWindow(WindowType.Tab);
+           driver.Navigate().GoToUrl(url);
+         }
         private void CloseCurrentTabAndSwitchBack(IWebDriver driver, string originalWindow)
-        {
+       {
             driver.Close();
-            driver.SwitchTo().Window(originalWindow);
-        }
-    }
+           driver.SwitchTo().Window(originalWindow);
+       }
+   }
 }
--- ConsoleApp1/SCRAPERS/TicketekScraper.cs
+++ ConsoleApp1/SCRAPERS/TicketekScraper.cs
@@ -10,53 +10,56 @@ namespace EventScraperBackend
 {
     public class TicketekScraper : BaseScraper
     {
-        private EventDataExtractor dataExtractor;
-          public TicketekScraper(string driverPath, string websiteUrl, int timeoutSeconds)
-              : base(driverPath, websiteUrl, timeoutSeconds)
-           {
-                 dataExtractor = new EventDataExtractor();
-            }
-           public void ScrapeEvents()
-           {
-                 try {
+        private EventDataExtractor _dataExtractor;
+        public TicketekScraper(string driverPath, string websiteUrl, int timeoutSeconds)
+             : base(driverPath, websiteUrl, timeoutSeconds)
+        {
+            _dataExtractor = new EventDataExtractor();
+        }
+        public List<EventData> ScrapeEvents()
+        {
+            List<EventData> eventList = new List<EventData>();
+            try
+            {
+                // Buscar un elemento
+                IReadOnlyCollection<IWebElement> contenedoresIniciales = wait.Until(ExpectedConditions
+                   .PresenceOfAllElementsLocatedBy(By.CssSelector("div.tkt-artist-list-image-item.relative.col-xs-10")));
 
-                    // Buscar un elemento
-                        IReadOnlyCollection<IWebElement> contenedoresIniciales = wait.Until(ExpectedConditions
-                            .PresenceOfAllElementsLocatedBy(By.CssSelector("div.tkt-artist-list-image-item.relative.col-xs-10")));
+                Console.WriteLine($"Total de contenedores encontrados: {contenedoresIniciales.Count}");
 
-                    Console.WriteLine($"Total de contenedores encontrados: {contenedoresIniciales.Count}");
-
-                        foreach (var contenedorInicial in contenedoresIniciales)
-                        {
-                            IWebElement container = contenedorInicial;
-                                try
-                                {
-                                    var boton = container.FindElement(By.CssSelector("a"));
-                                    var url = boton.GetAttribute("href");
-
-                                      EventData eventData;
-                                       if (url != null){
-                                          eventData =  dataExtractor.ExtractData(container, driver, wait, url);
-                                            if(eventData != null) {
-                                           Console.WriteLine($"Elementos extraídos: Nombre: '{eventData.Name}', Imagen: '{eventData.ImageUrl}', URL: '{eventData.Url}', Imagen Detalle: '{eventData.ImageUrlDetail}', Descripcion: '{eventData.Description}', Fecha: '{eventData.Date}', Lugar: '{eventData.Place}', Direccion: '{eventData.FinallocationDiv}', URL Compra: '{eventData.BuyUrl}'");
-                                          }
-                                        }
-                                }
-                                catch (Exception ex)
-                                {
-                                    Console.WriteLine($"Error en iteración (general): {ex.Message}");
-                                }
-                            }
-
-                    }
-                catch (Exception ex)
+                foreach (var contenedorInicial in contenedoresIniciales)
                 {
-                    Console.WriteLine($"Ocurrio un error (general): {ex.Message}");
+                    IWebElement container = contenedorInicial;
+                    try
+                    {
+                        var boton = container.FindElement(By.CssSelector("a"));
+                        var url = boton.GetAttribute("href");
+
+                        if (url != null)
+                        {
+                            EventData eventData = _dataExtractor.ExtractData(container, driver, wait, url);
+                            if (eventData != null)
+                            {
+                                eventList.Add(eventData);
+                                Console.WriteLine($"Elementos extraídos: Nombre: '{eventData.Name}', Imagen: '{eventData.ImageUrl}', URL: '{eventData.Url}', Imagen Detalle: '{eventData.ImageUrlDetail}', Descripcion: '{eventData.Description}', Fecha: '{eventData.Date}', Lugar: '{eventData.Place}', Direccion: '{eventData.FinallocationDiv}', URL Compra: '{eventData.BuyUrl}'");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error en iteración (general): {ex.Message}");
+                    }
                 }
-               finally
-               {
-                 Dispose();
-                }
-           }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocurrio un error (general): {ex.Message}");
+            }
+            finally
+            {
+                Dispose();
+            }
+            return eventList;
+        }
     }
 }
